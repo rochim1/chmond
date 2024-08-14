afd4fbd1ee3c0e41b9cefc3d3dffdb2fe4f44c84
rochim1 <muhammad.0206@students.amikom.ac.id>
2024-08-14 11:21:44 +0700

    create middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
+
+// Example global middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.use('/api', userRoutes);
 
 module.exports = app;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const app = express();
 const userController = require('../controllers/userController');
-
+// validator
 const userValidator = require('../validations/userValidator');
 const loginValidator = require('../validations/loginValidator');
-// const router = express.Router();
+
+// middleware
+const authMiddleware = require('../middlewares/authMiddleware')
 
 const apiRouter = express.Router();
-apiRouter.get('/users', userController.getAllUsers);
-
-apiRouter.post('/users/log_access', userController.logUserAccess); // Create a new user
 
 // User Routes
 apiRouter.post('/login', loginValidator, userController.login); // Login user
 apiRouter.post('/auth/google', userController.createUserByGoogle); // Authenticate with Google
 
+apiRouter.get('/users', userController.getAllUsers);
+apiRouter.post('/users/log_access', authMiddleware, userController.logUserAccess); // Create a new user
 apiRouter.get('/users', userController.getAllUsers); // View all user
 apiRouter.get('/users/:id_user', userController.getOneUsers); // View user
 apiRouter.post('/users/create', userValidator, userController.createUser); // Create a new user
